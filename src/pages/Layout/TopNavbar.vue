4cd5046f35d4a00b27a61ceae4972b33152d9caa
root <jdcanado@gmail.com>
2020-01-12 19:47:19 -0300

    Testando alterações pelo vim

--- src/pages/Layout/TopNavbar.vue
+++ src/pages/Layout/TopNavbar.vue
@@ -22,7 +22,7 @@
               v-model="selectedEmployee"
               :md-options="employees"
             >
-              <label>Search...</label>
+              <label>Pesquisar...</label>
             </md-autocomplete>
           </div>
           <md-list>
